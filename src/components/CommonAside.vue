4ed05f75e0f3a75854c75568300d81d74669b36b
awangdefa <249558764@qq.com>
2020-04-29 14:48:11 +0800

    init

---
+++ src/components/CommonAside.vue
@@ -0,0 +1,104 @@
+<template>
+  <!-- 主菜单 -->
+  <el-menu
+    default-active="2"
+    class="el-menu-vertical-demo"
+    background-color="#33aef0"
+    text-color="#fff"
+    active-text-color="#ffd04b"
+  >
+    <!-- 项目 -->
+    <el-menu-item
+      :index="item.path"
+      v-for="item in noChildren"
+      :key="item.path"
+    >
+      <i :class="'el-icon-' + item.icon"></i>
+      <span slot="title">{{ item.label }}</span>
+    </el-menu-item>
+    <!-- 多级菜单 -->
+    <el-submenu
+      index="item.path"
+      v-for="(item, index) in hasChildren"
+      :key="index"
+    >
+      <template slot="title">
+        <i class="el-icon-location"></i>
+        <span>{{ item.label }}</span>
+      </template>
+      <!-- 菜单项目组 -->
+      <el-menu-item-group>
+        <!-- 项目 -->
+        <el-menu-item
+          :index="subItem.path"
+          v-for="(subItem, subIndex) in item.children"
+          :key="subIndex"
+        >
+          {{ subItem.label }}</el-menu-item
+        >
+      </el-menu-item-group>
+    </el-submenu>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  name: "common-aside",
+  computed: {
+    noChildren() {
+      return this.asideMenu.filter(item => !item.children);
+    },
+    hasChildren() {
+      return this.asideMenu.filter(item => item.children);
+    }
+  },
+  data: () => ({
+    asideMenu: [
+      {
+        path: "/",
+        name: "home",
+        label: "首页",
+        icon: "s-home"
+      },
+      {
+        path: "/video",
+        name: "video",
+        label: "视频管理",
+        icon: "video-play"
+      },
+      {
+        path: "/user",
+        name: "user",
+        label: "用户管理",
+        icon: "user"
+      },
+
+      {
+        label: "其他",
+        icon: "user",
+        children: [
+          {
+            path: "/page1",
+            name: "page1",
+            label: "页面1",
+            icon: "setting"
+          },
+          {
+            path: "/page2",
+            name: "page2",
+            label: "页面2",
+            icon: "setting"
+          }
+        ]
+      }
+    ]
+  })
+};
+</script>
+
+<style lang="scss" scoped>
+.el-menu {
+  height: 100%;
+  border: none;
+}
+</style>
---
+++ src/components/CommonHeader.vue
@@ -0,0 +1,65 @@
+<template>
+  <header>
+    <div class="l-content">
+      <el-button type="primary" icon="el-icon-menu" size="mini"></el-button>
+      <!-- 面包屑导航 -->
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+    <div class="r-content">
+      <!-- 下拉菜单 -->
+      <el-dropdown trigger="click">
+        <span class="el-dropdown-link"
+          ><img :src="userImg" class="userImg" />
+        </span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item icon="el-icon-plus">个人中心</el-dropdown-item>
+          <el-dropdown-item icon="el-icon-circle-plus">退出</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "common-header",
+  data: () => ({
+    userImg: require("@/assets/images/userImg.gif")
+  })
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  display: flex;
+  height: 100%;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.l-content {
+  display: flex;
+  align-items: center;
+  .el-button {
+    margin-right: 20px;
+  }
+}
+
+.r-content {
+  .userImg {
+    width: 40px;
+    height: 40px;
+    border-radius: 8px;
+  }
+}
+</style>
+
+<style lang="scss">
+.el-breadcrumb__inner a,
+.el-breadcrumb__inner.is-link {
+  color: white;
+}
+</style>
